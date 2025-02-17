24d787d9e5d43fb6fc2b90737809d5f3e28f462b
Robin <robindev2019@outlook.com>
2025-02-17 19:29:00 +0100

    Setup build pipeline with proper minification

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - [ ] mobile support using touch events
 - [ ] online highscore using the JS `fetch` API
 - [ ] increase block speed by the logarithm of the score
+- [ ] make the enemies triangles
+- [ ] make the player a balloon
---
+++ index.js
@@ -0,0 +1,80 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const font = "18px monospace";
+const keys_pressed = {};
+
+let player = { x: 140, y: 280, w: 20, h: 20, speed: 5 };
+let enemies = [];
+let tick = 0;
+let spawn_interval = 30;
+let score = 0;
+let game_over = false;
+
+window.addEventListener("keydown", (e) => (keys_pressed[e.keyCode] = 1));
+window.addEventListener("keyup", (e) => (keys_pressed[e.keyCode] = 0));
+
+(function L() {
+  if (game_over) {
+    ctx.fillStyle = "#c5ff8c";
+    ctx.font = font;
+    ctx.fillText("DEAD", 120, 165);
+
+    if (keys_pressed[32]) {
+      g = false;
+      score = 0;
+    }
+
+    return;
+  }
+
+  if (keys_pressed[37] && player.x > 0) player.x -= player.speed;
+  if (keys_pressed[39] && player.x < canvas.width - player.w)
+    player.x += player.speed;
+
+  tick++;
+
+  if (tick >= spawn_interval) {
+    tick = 0;
+
+    enemies.push({
+      x: Math.random() * (canvas.width - 20),
+      y: -20,
+      w: 20,
+      h: 20,
+      s: 3 + Math.random() * 2,
+    });
+  }
+
+  for (let i = 0; i < enemies.length; i++) {
+    enemies[i].y += enemies[i].s;
+
+    if (
+      enemies[i].x < player.x + player.w &&
+      enemies[i].x + enemies[i].w > player.x &&
+      enemies[i].y < player.y + player.h &&
+      enemies[i].y + enemies[i].h > player.y
+    )
+      game_over = true;
+  }
+
+  enemies = enemies.filter((e) =>
+    e.y <= canvas.height ? true : (score++, false)
+  );
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw player
+  ctx.fillStyle = "#38ba8b";
+  ctx.fillRect(player.x, player.y, player.w, player.h);
+
+  // Draw enemies
+  ctx.fillStyle = "#227a7a";
+  enemies.forEach((e) => ctx.fillRect(e.x, e.y, e.w, e.h));
+
+  // Draw score
+  ctx.fillStyle = "#c5ff8c";
+  ctx.font = font;
+  ctx.fillText(score, 10, 20);
+
+  requestAnimationFrame(L);
+})();
