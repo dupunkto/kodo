b58cbf018660596e6a25a547289c4104eadc0272
Robin <robindev2019@outlook.com>
2025-02-21 17:40:56 +0100

    Refactor coin and enemy code
    - Consistent naming
    - Condense multiple loops into one
    - Fix bug with skipped indexes for coins
    - Use less bytes for drawing
    - Abstract common code for spawning

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const fetch_api = async (endpoint) => {
 
 let high_score = await fetch_api("/get");
 let player = { x: 140, y: 280, w: 20, h: 20 };
-let enemies = [];
 let coins = [];
-let tick = 0;
+let enemies = [];
 let coin_tick = 0;
+let enemy_tick = 0;
 let score = 0;
 let game_over = false;
 let mouse_down = false;
@@ -40,13 +40,14 @@ bind_event("mousemove", (e) => {
 });
 
 // Will gradually decrease
-let spawn_interval = 31;
+let enemy_interval = 31;
 let coin_interval = 120;
+
 const movement_speed = 400;
 const base_speed = 300;
 const speed_multiplier = 600;
-const speed_variance = 120;
 const speed_variance_coin = 80;
+const speed_variance_enemy = 120;
 
 async function L(ctime) {
   let dt = (ctime - ltime) / 1000;
@@ -87,87 +88,75 @@ async function L(ctime) {
     if (keys_pressed[37]) player.x -= movement_speed * dt;
     if (keys_pressed[39]) player.x += movement_speed * dt;
 
+    // Make sure the player cannot teleport out of the screen.
     player.x = Math.max(0, Math.min(canvas.width - player.w, player.x));
 
-    tick += dt * 60;
     coin_tick += dt * 60;
+    enemy_tick += dt * 60;
 
-    // So it doesn't go to low
-    spawn_interval = Math.max(15, spawn_interval - Math.random() * (0.01 - 0.001) + 0.001);
+    // Gradually decrease spawn interval but ensure
+    // it doesn't drop below a minimum value.
+    enemy_interval = Math.max(15, enemy_interval - Math.random() * (0.01 - 0.001) + 0.001);
 
-    if (tick >= spawn_interval) {
-      tick = 0;
-
+    const spawn = (collection, size, speed_variance) => {
       let r_component = Math.random() * speed_variance;
       let l_component = Math.sqrt(score * speed_multiplier);
 
-      enemies.push({
-        x: Math.random() * (canvas.width - 20),
+      const random_speed = base_speed + r_component + l_component;
+      const random_position = Math.random() * (canvas.width - 20);
+
+      collection.push({
+        x: random_position,
         y: -20,
-        w: 20,
-        h: 20,
-        s: base_speed + r_component + l_component,
+        w: size,
+        h: size,
+        s: random_speed,
       });
+    }
+
+    if (enemy_tick >= enemy_interval) {
+      spawn(enemies, 20, speed_variance_enemy);
+      enemy_tick = 0;
     }
 
     if (coin_tick >= coin_interval) {
+      spawn(coins, 10, speed_variance_coin);
       coin_tick = 0;
-
-      let r_component = Math.random() * speed_variance_coin;
-      let l_component = Math.sqrt(score * speed_variance_coin);
-
-      coins.push({
-        x: Math.random() * (canvas.width - 20),
-        y: -20,
-        w: 10,
-        h: 10,
-        s: base_speed + r_component + l_component,
-      });
     }
 
-    for (let i = 0; i < enemies.length; i++) {
-      enemies[i].y += enemies[i].s * dt;
-
-      if (
-        enemies[i].x < player.x + player.w &&
-        enemies[i].x + enemies[i].w > player.x &&
-        enemies[i].y < player.y + player.h &&
-        enemies[i].y + enemies[i].h > player.y
-      )
-        game_over = true;
-    }
-
-    for (let i = 0; i < coins.length; i++) {
-      coins[i].y += coins[i].s * dt;
-
-      if (
-        coins[i].x < player.x + player.w &&
-        coins[i].x + coins[i].w > player.x &&
-        coins[i].y < player.y + player.h &&
-        coins[i].y + coins[i].h > player.y
-      ) {
-        score += 2;
-        coins.splice(i, 1);
-      }
-    }
-
-    enemies = enemies.filter((e) =>
-      e.y <= canvas.height ? true : (score++, false)
+    const collide = (a, b) => (
+      a.x < b.x + b.w &&
+      a.x + a.w > b.x &&
+      a.y < b.y + b.h &&
+      a.y + a.h > b.y
     );
+
+    enemies = enemies.filter((enemy) => {
+      enemy.y += enemy.s * dt;
+      if(collide(enemy, player)) game_over = true;
+      return enemy.y <= canvas.height ? true : (score++, false)
+    });
+
+    coins = coins.filter((coin) => {
+      coin.y += coin.s * dt;
+      return collide(coin, player) ? (score += 2, false) : true;
+    });
+
+    const draw = (e) => ctx.fillRect(e.x, e.y, e.w, e.h);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw player
     ctx.fillStyle = "#38ba8b";
-    ctx.fillRect(player.x, player.y, player.w, player.h);
+    draw(player);
 
     // Draw enemies
     ctx.fillStyle = "#227a7a";
-    enemies.forEach((e) => ctx.fillRect(e.x, e.y, e.w, e.h));
+    enemies.forEach(draw);
 
     // Draw coins
     ctx.fillStyle = "#f8e86d";
-    coins.forEach((e) => ctx.fillRect(e.x, e.y, e.w, e.h));
+    coins.forEach(draw);
 
     // Draw score
     ctx.fillStyle = "#c5ff8c";
@@ -177,6 +166,6 @@ async function L(ctime) {
   }
 
   requestAnimationFrame(L);
-}
+};
 
 requestAnimationFrame(L);
